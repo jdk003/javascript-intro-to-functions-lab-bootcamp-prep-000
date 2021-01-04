651c305aff482ccb4c2f53f005cfa541447ffbe8
Jessica Kim <jdk003@ucsd.edu>
2021-01-04 17:53:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(str) {
+  return str.toUpperCase()
+}
+
+function whisper(str) {
+  return str.toUpperCase()
+}
+
+function logShout(str) {
+  console.log(str.toUpperCase())
+}
+
+function shout(str) {
+  return str.toUpperCase()
+}
